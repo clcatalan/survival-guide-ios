07408a154adcc793b6a83ce1378a6eadfa3ffda3
Carlos Catalan <crlsctalan23@gmail.com>
2019-07-07 22:48:42 +0800

    initial setup

---
+++ SurvivalGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SurvivalGuide
+//
+//  Created by Carlos Catalan on 07/07/2019.
+//  Copyright © 2019 Carlos Catalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
